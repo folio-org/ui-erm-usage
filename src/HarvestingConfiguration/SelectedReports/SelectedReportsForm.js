3ebe2cc4fde96300dc30bb52efddf72a54e03f7f
Richard Redweik <redweik@ub.uni-leipzig.de>
2018-10-12 10:56:22 +0200

    Update RepeatableField to new stripes version

--- src/HarvestingConfiguration/SelectedReports/SelectedReportsForm.js
+++ src/HarvestingConfiguration/SelectedReports/SelectedReportsForm.js
@@ -1,43 +1,42 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Field } from 'redux-form';
+import React, { Fragment } from 'react';
+import { Field, FieldArray } from 'redux-form';
 import Select from '@folio/stripes-components/lib/Select';
 import RepeatableField from '@folio/stripes-components/lib/RepeatableField';
 import counter4Reports from './data/counter4Reports';
 import counter5Reports from './data/counter5Reports';
 
-const renderSelectedReport = ({ field, fieldIndex }) => {
-  const counter4Options = counter4Reports.selectedOptions(field);
-  const counter5Options = counter5Reports.selectedOptions(field);
-  return (
-    <Field
-      label={fieldIndex === 0 ? 'Select reports' : null}
-      name={`${field}`}
-      component={Select}
-      dataOptions={[
-        { label: 'Counter 5 Reports', value: '', disabled: true },
-        ...counter5Options,
-        { label: 'Counter 4 Reports', value: '', disabled: true },
-        ...counter4Options
-      ]}
-    />
-  );
-};
+class SelectedReportsForm extends React.Component {
+  renderField = (identifier, index) => {
+    const counter4Options = counter4Reports.getOptions();
+    const counter5Options = counter5Reports.getOptions();
+    return (
+      <Fragment>
+        <Field
+          label={index === 0 ? 'Select reports' : null}
+          name={identifier}
+          type="text"
+          component={Select}
+          dataOptions={[
+            { label: 'Counter 4 Reports', value: '', disabled: true },
+            ...counter4Options,
+            { label: 'Counter 5 Reports', value: '', disabled: true },
+            ...counter5Options
+          ]}
+        />
+      </Fragment>
+    );
+  }
 
-renderSelectedReport.propTypes = {
-  field: PropTypes.object,
-  fieldIndex: PropTypes.number,
-};
-
-const SelectedReportsForm = () => (
-  <RepeatableField
-    name="requestedReports"
-    addLabel="Add reports"
-    addButtonId="clickable-add-report"
-    template={[{
-        render(fieldObj) { return renderSelectedReport(fieldObj); },
-      }]}
-  />
-);
+  render() {
+    return (
+      <FieldArray
+        addLabel="+ Add report"
+        component={RepeatableField}
+        name="requestedReports"
+        renderField={this.renderField}
+      />
+    );
+  }
+}
 
 export default SelectedReportsForm;
