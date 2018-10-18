a2e6a08ce3918018e8b42c6231a851cb0d0b4921
Richard Redweik <redweik@ub.uni-leipzig.de>
2018-10-18 12:17:36 +0200

    Bugfix RepeatableField in ContactsForm

--- src/Settings/Aggregators/DisplayContactsForm.js
+++ src/Settings/Aggregators/DisplayContactsForm.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import TextField from '@folio/stripes-components/lib/TextField';
 import RepeatableField from '@folio/stripes-components/lib/RepeatableField';
+import { Field, FieldArray } from 'redux-form';
 
-const DisplayContactsForm = () => (
-  <RepeatableField
-    name="accountConfig.displayContact"
-    addLabel="Add Display Contact"
-    addButtonId="clickable-add-language"
-    template={[{
-      component: TextField,
-    }]}
-  />
-);
+class DisplayContactsForm extends React.Component {
+  renderField = (identifier) => {
+    return (
+      <Fragment>
+        <Field
+          label="Add Display Contact"
+          name={identifier}
+          id="input-display-contact"
+          component={TextField}
+        />
+      </Fragment>
+    );
+  }
+
+  render() {
+    return (
+      <FieldArray
+        addLabel="+ Add Display Contact"
+        component={RepeatableField}
+        name="accountConfig.displayContact"
+        renderField={this.renderField}
+      />
+    );
+  }
+}
+
+// const DisplayContactsForm = () => (
+//   <RepeatableField
+//     name="accountConfig.displayContact"
+//     addLabel="Add Display Contact"
+//     addButtonId="clickable-add-language"
+//     template={[{
+//       component: TextField,
+//     }]}
+//   />
+// );
 
 export default DisplayContactsForm;
