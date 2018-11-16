4ec441452fc362ba4c13153edbcc2b2d77c13df7
Richard Redweik <redweik@ub.uni-leipzig.de>
2018-11-16 08:22:27 +0100

    Add button to start harvesting all udps for tenant

--- src/Settings/general-settings.js
+++ src/Settings/general-settings.js
@@ -1,17 +1,52 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Pane } from '@folio/stripes/components';
+import { 
+  Button,
+  Pane
+} from '@folio/stripes/components';
 import { FormattedMessage } from 'react-intl';
 
 export default class GeneralSettings extends React.Component {
+  static manifest = Object.freeze({
+    harvesterStart: {
+      type: 'okapi',
+      fetch: false,
+      accumulate: 'true',
+      GET: {
+        path: 'harvester/start',
+      },
+    }
+  });
+
   static propTypes = {
     label: PropTypes.string.isRequired,
+    mutator: PropTypes.shape({
+      harvesterStart: PropTypes.object,
+    }),
   };
 
+  onClickStartHarvester = () => {
+    this.props.mutator.harvesterStart.GET().then(() => {
+      console.log('Started harvester');
+    });
+  }
+
   render() {
+
+    const startHarvesterButton = (
+      <Button
+        onClick={() => this.onClickStartHarvester()}
+      >
+        { 'Start harvesting' }
+      </Button>
+    );
+
     return (
       <Pane defaultWidth="fill" fluidContentWidth paneTitle={this.props.label}>
-        <div id="stripes-new-app-settings-message"><FormattedMessage id="ui-erm-usage.settings.general.message" /></div>
+        <div>
+          {'Start the harvester for the current tenant: '}
+          { startHarvesterButton }
+        </div>
       </Pane>
     );
   }
