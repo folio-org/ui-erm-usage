5658a334218f94cdc66c8fb403e6d657ef600e30
Richard Redweik <redweik@ub.uni-leipzig.de>
2020-12-08 15:50:28 +0100

    Restructure custom report info

--- src/components/Statistics/Custom/CustomReportInfo/CustomReportInfoFile.js
+++ src/components/Statistics/Custom/CustomReportInfo/CustomReportInfoFile.js
@@ -2,59 +2,42 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import { Button, Icon, KeyValue, MenuSection } from '@folio/stripes-components';
-import { ViewMetaData } from '@folio/stripes-smart-components';
+
+import ReportInfoHeader from './ReportInfoHeader';
 
 function CustomReportInfoFile(props) {
   const { customReport, handlers, onDelete, udpLabel } = props;
-  const headerSection = (
-    <MenuSection id="menu-actions" label="Custom Report Info" labelTag="h3">
-      <ViewMetaData metadata={customReport.metadata} />
-      <KeyValue
-        label={<FormattedMessage id="ui-erm-usage.usage-data-provider" />}
-        value={udpLabel}
-      />
-      <KeyValue
-        label={<FormattedMessage id="ui-erm-usage.general.note" />}
-        value={customReport.note}
-      />
-      <KeyValue
-        label={<FormattedMessage id="ui-erm-usage.general.year" />}
-        value={customReport.year}
-      />
-      <KeyValue
-        label={<FormattedMessage id="ui-erm-usage.general.fileSize.kb" />}
-        value={customReport.fileSize}
-      />
-    </MenuSection>
-  );
-
-  const actionSection = (
-    <MenuSection id="menu-actions" label="Actions" labelTag="h3">
-      <Button
-        id="download-custom-report-button"
-        buttonStyle="dropdownItem"
-        onClick={() =>
-          handlers.doDownloadFile(customReport.fileId, customReport.fileName)
-        }
-      >
-        <Icon icon="arrow-down">{`Download ${customReport.fileName}`}</Icon>
-      </Button>
-      <Button
-        id="delete-custom-report-button"
-        buttonStyle="dropdownItem"
-        onClick={onDelete}
-      >
-        <Icon icon="trash">
-          <FormattedMessage id="ui-erm-usage.statistics.custom.delete" />
-        </Icon>
-      </Button>
-    </MenuSection>
-  );
 
   return (
     <>
-      {headerSection}
-      {actionSection}
+      <ReportInfoHeader customReport={customReport} udpLabel={udpLabel}>
+        <KeyValue
+          label={<FormattedMessage id="ui-erm-usage.general.year" />}
+          value={customReport.year}
+        />
+        <KeyValue
+          label={<FormattedMessage id="ui-erm-usage.general.fileSize.kb" />}
+          value={customReport.fileSize}
+        />
+      </ReportInfoHeader>
+      <MenuSection id="menu-actions" label="Actions" labelTag="h3">
+        <Button
+          id="download-custom-report-button"
+          buttonStyle="dropdownItem"
+          onClick={() => handlers.doDownloadFile(customReport.fileId, customReport.fileName)}
+        >
+          <Icon icon="arrow-down">{`Download ${customReport.fileName}`}</Icon>
+        </Button>
+        <Button
+          id="delete-custom-report-button"
+          buttonStyle="dropdownItem"
+          onClick={onDelete}
+        >
+          <Icon icon="trash">
+            <FormattedMessage id="ui-erm-usage.statistics.custom.delete" />
+          </Icon>
+        </Button>
+      </MenuSection>
     </>
   );
 }
--- src/components/Statistics/Custom/CustomReportInfo/CustomReportInfoLink.js
+++ src/components/Statistics/Custom/CustomReportInfo/CustomReportInfoLink.js
@@ -1,55 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
-import {
-  Button,
-  Icon,
-  KeyValue,
-  MenuSection,
-  TextLink,
-} from '@folio/stripes-components';
-import { ViewMetaData } from '@folio/stripes-smart-components';
+import { Button, Icon, MenuSection, TextLink } from '@folio/stripes-components';
+
+import ReportInfoHeader from './ReportInfoHeader';
 
 function CustomReportInfoLink(props) {
   const { customReport, onDelete, udpLabel } = props;
-  const headerSection = (
-    <MenuSection id="menu-actions" label="Custom Report Info" labelTag="h3">
-      <ViewMetaData metadata={customReport.metadata} />
-      <KeyValue
-        label={<FormattedMessage id="ui-erm-usage.usage-data-provider" />}
-        value={udpLabel}
-      />
-      <KeyValue
-        label={<FormattedMessage id="ui-erm-usage.general.note" />}
-        value={customReport.note}
-      />
-    </MenuSection>
-  );
-  const actionSection = (
-    <MenuSection id="menu-actions" label="Actions" labelTag="h3">
-      <TextLink
-        target="_blank"
-        rel="noopener noreferrer"
-        href={customReport.linkUrl}
-      >
-        <Icon icon="external-link">{customReport.linkUrl}</Icon>
-      </TextLink>
-      <Button
-        id="delete-custom-report-button"
-        buttonStyle="dropdownItem"
-        onClick={onDelete}
-      >
-        <Icon icon="trash">
-          <FormattedMessage id="ui-erm-usage.statistics.custom.delete" />
-        </Icon>
-      </Button>
-    </MenuSection>
-  );
 
   return (
     <>
-      {headerSection}
-      {actionSection}
+      <ReportInfoHeader customReport={customReport} udpLabel={udpLabel} />
+      <MenuSection id="menu-actions" label="Actions" labelTag="h3">
+        <TextLink
+          target="_blank"
+          rel="noopener noreferrer"
+          href={customReport.linkUrl}
+        >
+          <Icon icon="external-link">{customReport.linkUrl}</Icon>
+        </TextLink>
+        <Button
+          id="delete-custom-report-button"
+          buttonStyle="dropdownItem"
+          onClick={onDelete}
+        >
+          <Icon icon="trash">
+            <FormattedMessage id="ui-erm-usage.statistics.custom.delete" />
+          </Icon>
+        </Button>
+      </MenuSection>
     </>
   );
 }
