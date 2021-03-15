1fb3bbc92e2beb9ccf2d544c61917d5b20178ea2
rchr <richard.redweik@posteo.de>
2021-03-15 16:47:07 +0100

    Removed unused useState hook

--- src/routes/UDPViewRoute.js
+++ src/routes/UDPViewRoute.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import compose from 'compose-function';
 import { get, isEmpty } from 'lodash';
