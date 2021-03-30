44fd15fcaee9ac05a2d693a244f0aa998cf80186
rchr <richard.redweik@posteo.de>
2021-03-30 09:19:44 +0200

    Test UDPs

---
+++ test/fixtures/aggregator.js
@@ -0,0 +1,12 @@
+const aggregator = {
+  id: 'b02d9da7-631d-4252-81c0-52d2a048bb4e',
+  label: 'Aggregator Test',
+  serviceType: 'NSS',
+  serviceUrl: 'http://aggregagtor.de',
+  accountConfig: {
+    configType: 'API',
+    displayContact: [],
+  },
+};
+
+export default aggregator;
