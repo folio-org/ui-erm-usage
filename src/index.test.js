7462fa40915a304fa61f251f366cb35103e0efa8
Viola Elsenhans <elsenhans@ub.uni-leipzig.de>
2021-04-29 10:30:29 +0200

    UIEUS-260-add-keyboard-shortcut-modal add index mock test

--- src/index.test.js
+++ src/index.test.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { act } from '@testing-library/react';
+import { act, render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 
 import { useStripes } from '@folio/stripes/core';
 import renderWithIntl from '../test/jest/helpers';
 import UDPEditRoute from './routes/UDPEditRoute';
 import UDPCreateRoute from './routes/UDPCreateRoute';
+import ErmUsage from './index';
 
+const match = {
+  isExact: false,
+  params: {},
+  path: '/eusage',
+  url: '/eusage',
+};
+
+const register = jest.fn();
+const deregister = jest.fn();
 
 const editRouteProps = {
   history: {
@@ -33,6 +43,10 @@ const renderWithRouter = (component) => {
   });
 };
 
+jest.mock('./index', () => {
+  return () => <span>eUsage</span>;
+});
+
 let stripes;
 describe('index', () => {
   beforeEach(() => {
@@ -47,5 +61,19 @@ describe('index', () => {
   it('should render UDPCreateRoute', () => {
     renderWithRouter(<UDPCreateRoute stripes={stripes} {...editRouteProps} />);
     expect(document.querySelector('#form-udp')).toBeInTheDocument();
+  });
+});
+
+describe('Application root', () => {
+  it('should render without crashing', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <ErmUsage match={match} register={register} deregister={deregister} />
+      </MemoryRouter>
+    );
+    const div = document.createElement('div');
+    div.id = 'root';
+    document.body.appendChild(div);
+    expect(getByText('eUsage')).toBeDefined();
   });
 });
