5d66f2db3dd6bb6e13393c646110a736d2d2a510
elsenhans <48911833+elsenhans@users.noreply.github.com>
2022-09-12 12:24:18 +0200

    UIEUS-301-set-default-values-for-create-udp (#386)
    Co-authored-by: elsenhans <elsenhans@users.noreply.github.com>

--- src/components/HarvestingConfiguration/Fields/HarvestingStatusSelect.js
+++ src/components/HarvestingConfiguration/Fields/HarvestingStatusSelect.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Field } from 'react-final-form';
-import { FormattedMessage, useIntl } from 'react-intl';
+import { FormattedMessage } from 'react-intl';
 import { Select } from '@folio/stripes/components';
 import { required } from '../../../util/validate';
 
@@ -8,22 +8,17 @@ import harvestingStatusOptions from '../../../util/data/harvestingStatusOptions'
 import useTranslateLabels from '../../../util/hooks/useTranslateLabels';
 
 const HarvestingStatusSelect = () => {
-  const intl = useIntl();
   return (
     <Field
-      label={
-        <FormattedMessage id="ui-erm-usage.udpHarvestingConfig.harvestingStatus" />
-      }
-      name="harvestingConfig.harvestingStatus"
-      id="addudp_harvestingstatus"
-      placeholder={intl.formatMessage({
-        id: 'ui-erm-usage.udp.form.placeholder.harvestingStatus',
-      })}
       component={Select}
       dataOptions={useTranslateLabels(harvestingStatusOptions)}
+      defaultValue="active"
+      fullWidth
+      id="addudp_harvestingstatus"
+      label={<FormattedMessage id="ui-erm-usage.udpHarvestingConfig.harvestingStatus" />}
+      name="harvestingConfig.harvestingStatus"
       required
       validate={required}
-      fullWidth
     />
   );
 };
