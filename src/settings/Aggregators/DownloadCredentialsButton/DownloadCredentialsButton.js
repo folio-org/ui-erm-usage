97d93e439465dc0fd11c4776ff11d3e25a416e7e
alb3rtino <alb3rtino@users.noreply.github.com>
2023-10-27 16:11:36 +0200

    Convert to functional component

--- src/settings/Aggregators/DownloadCredentialsButton/DownloadCredentialsButton.js
+++ src/settings/Aggregators/DownloadCredentialsButton/DownloadCredentialsButton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
-import { stripesConnect } from '@folio/stripes/core';
+import { stripesConnect, stripesShape } from '@folio/stripes/core';
 import {
   Button,
   Dropdown,
@@ -11,49 +11,47 @@ import {
 
 import { downloadCredentials } from '../../../util/downloadReport';
 
-class DownloadCredentialsButton extends React.Component {
-  static propTypes = {
-    aggregatorId: PropTypes.string.isRequired,
-    stripes: PropTypes.shape().isRequired,
-  };
-
-  onClickDownloadCredentials = (format) => {
+const DownloadCredentialsButton = ({ aggregatorId, stripes }) => {
+  const onClickDownloadCredentials = (format) => {
     downloadCredentials(
-      this.props.aggregatorId,
+      aggregatorId,
       format,
-      this.props.stripes.okapi,
+      stripes.okapi,
       { 'Content-Type': 'application/json' },
     ).catch((err) => {
-      this.log(err.message);
+      console.log(err.message);
     });
   };
 
-  render() {
-    return (
-      <Dropdown
-        label={<FormattedMessage id="ui-erm-usage.settings.credentials.download" />}
-      >
-        <DropdownMenu>
-          <Button
-            buttonStyle="dropdownItem"
-            onClick={() => this.onClickDownloadCredentials('csv')}
-          >
-            <Icon icon="arrow-down">
-              <FormattedMessage id="ui-erm-usage.settings.credentials.download.csv" />
-            </Icon>
-          </Button>
-          <Button
-            buttonStyle="dropdownItem"
-            onClick={() => this.onClickDownloadCredentials('xlsx')}
-          >
-            <Icon icon="arrow-down">
-              <FormattedMessage id="ui-erm-usage.settings.credentials.download.xlsx" />
-            </Icon>
-          </Button>
-        </DropdownMenu>
-      </Dropdown>
-    );
-  }
-}
+  return (
+    <Dropdown
+      label={<FormattedMessage id="ui-erm-usage.settings.credentials.download" />}
+    >
+      <DropdownMenu>
+        <Button
+          buttonStyle="dropdownItem"
+          onClick={() => onClickDownloadCredentials('csv')}
+        >
+          <Icon icon="arrow-down">
+            <FormattedMessage id="ui-erm-usage.settings.credentials.download.csv" />
+          </Icon>
+        </Button>
+        <Button
+          buttonStyle="dropdownItem"
+          onClick={() => onClickDownloadCredentials('xlsx')}
+        >
+          <Icon icon="arrow-down">
+            <FormattedMessage id="ui-erm-usage.settings.credentials.download.xlsx" />
+          </Icon>
+        </Button>
+      </DropdownMenu>
+    </Dropdown>
+  );
+};
+
+DownloadCredentialsButton.propTypes = {
+  aggregatorId: PropTypes.string.isRequired,
+  stripes: stripesShape.isRequired,
+};
 
 export default stripesConnect(DownloadCredentialsButton);
