eece474dde88785182238e643563fd02afe76c30
alb3rtino <alb3rtino@users.noreply.github.com>
2024-03-15 07:40:52 +0100

    Fix translation key

--- src/commands.js
+++ src/commands.js
@@ -4,32 +4,32 @@ import { FormattedMessage } from 'react-intl';
 const commands = [
   {
     name: 'new',
-    label: (<FormattedMessage id="stripes-component.shortcut.createRecord" />),
+    label: (<FormattedMessage id="stripes-components.shortcut.createRecord" />),
     shortcut: 'alt+n'
   },
   {
     name: 'edit',
-    label: (<FormattedMessage id="stripes-component.shortcut.editRecord" />),
+    label: (<FormattedMessage id="stripes-components.shortcut.editRecord" />),
     shortcut: 'mod+alt+e'
   },
   {
     name: 'save',
-    label: (<FormattedMessage id="stripes-component.shortcut.saveRecord" />),
+    label: (<FormattedMessage id="stripes-components.shortcut.saveRecord" />),
     shortcut: 'mod+s'
   },
   {
     name: 'expandAllSections',
-    label: (<FormattedMessage id="stripes-component.shortcut.expandAll" />),
+    label: (<FormattedMessage id="stripes-components.shortcut.expandAll" />),
     shortcut: 'mod+alt+b'
   },
   {
     name: 'collapseAllSections',
-    label: (<FormattedMessage id="stripes-component.shortcut.collapseAll" />),
+    label: (<FormattedMessage id="stripes-components.shortcut.collapseAll" />),
     shortcut: 'mod+alt+g'
   },
   {
     name: 'search',
-    label: (<FormattedMessage id="stripes-component.shortcut.goToSearchFilter" />),
+    label: (<FormattedMessage id="stripes-components.shortcut.goToSearchFilter" />),
     shortcut: 'mod+alt+h'
   }
 ];
