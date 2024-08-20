c4e900f7de63afe4047958a44832c6bca6b2fc32
elsenhans <elsenhans@users.noreply.github.com>
2024-08-20 12:17:45 +0200

    UIEUS-362-use-functional-components

--- src/components/HarvestingConfiguration/SelectedReports/SelectedReportsForm.js
+++ src/components/HarvestingConfiguration/SelectedReports/SelectedReportsForm.js
@@ -1,73 +1,78 @@
+import _ from 'lodash';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { filter, isNaN } from 'lodash';
-import { useState, useEffect } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { FieldArray } from 'react-final-form-arrays';
-
 import { Label } from '@folio/stripes/components';
-
 import formCss from '../../../util/sharedStyles/form.css';
 import counterReports from './data/counterReports';
 import css from './SelectedReportsForm.css';
+
 import SelectReportType from './SelectReportType';
 import { requiredArray } from '../../../util/validate';
 
 const getCounterReportsForVersion = (counterVersion) => {
-  return filter(counterReports.getOptions(), [
+  return _.filter(counterReports.getOptions(), [
     'counterVersion',
     '' + counterVersion,
   ]);
 };
 
-const SelectedReportsForm = ({
-  counterVersion,
-  selectedReports,
-  required,
-}) => {
-  const counterReportsForVersion = getCounterReportsForVersion(counterVersion);
-  const [counterReportsCurrentVersion, setCounterReportsCurrentVersion] = useState(counterReportsForVersion);
+class SelectedReportsForm extends React.Component {
+  static propTypes = {
+    counterVersion: PropTypes.number,
+    selectedReports: PropTypes.arrayOf(PropTypes.string),
+    required: PropTypes.bool
+  };
 
-  useEffect(() => {
-    const currCounterReportsForVersion = isNaN(counterVersion)
-      ? []
-      : getCounterReportsForVersion(counterVersion);
-    setCounterReportsCurrentVersion(currCounterReportsForVersion);
-  }, [counterVersion]);
+  constructor(props) {
+    super(props);
+    this.counterReports = counterReports.getOptions();
+    this.counterReportsForVersion = getCounterReportsForVersion(
+      props.counterVersion
+    );
+    this.counterReportsCurrentVersion = this.counterReportsForVersion;
+  }
 
-  const reportsSelect = (
-    <FieldArray
-      name="harvestingConfig.requestedReports"
-      required={required}
-      // dont know why, but this seems to work
-      validate={(value) => required && requiredArray(value)}
-    >
-      {({ fields }) => (
-        <SelectReportType
-          counterReportsCurrentVersion={counterReportsCurrentVersion}
-          fields={fields}
-          selectedReports={selectedReports}
-          required={required}
-        />
-      )}
-    </FieldArray>
-  );
+  componentDidUpdate(prevProps) {
+    if (this.props.counterVersion !== prevProps.counterVersion) {
+      const counterReportsForVersion = _.isNaN(this.props.counterVersion)
+        ? []
+        : getCounterReportsForVersion(this.props.counterVersion);
+      this.counterReportsCurrentVersion = counterReportsForVersion;
+    }
+  }
 
-  return (
-    <>
-      <div className={formCss.label}>
-        <Label required={required}>
-          <FormattedMessage id="ui-erm-usage.udpHarvestingConfig.requestedReport" />
-        </Label>
-      </div>
-      <div className={css.reportListDropdownWrap}>{reportsSelect}</div>
-    </>
-  );
-};
+  render() {
+    const reportsSelect = (
+      <FieldArray
+        name="harvestingConfig.requestedReports"
+        required={this.props.required}
+        // dont know why, but this seems to work
+        validate={(value) => this.props.required && requiredArray(value)}
+      >
+        {({ fields }) => (
+          <SelectReportType
+            counterReportsCurrentVersion={this.counterReportsCurrentVersion}
+            fields={fields}
+            selectedReports={this.props.selectedReports}
+            required={this.props.required}
+          />
+        )}
+      </FieldArray>
+    );
 
-SelectedReportsForm.propTypes = {
-  counterVersion: PropTypes.number,
-  selectedReports: PropTypes.arrayOf(PropTypes.string),
-  required: PropTypes.bool
-};
+    return (
+      <>
+        <div className={formCss.label}>
+          <Label required={this.props.required}>
+            <FormattedMessage id="ui-erm-usage.udpHarvestingConfig.requestedReport" />
+          </Label>
+        </div>
+        <div className={css.reportListDropdownWrap}>{reportsSelect}</div>
+      </>
+    );
+  }
+}
 
 export default SelectedReportsForm;
