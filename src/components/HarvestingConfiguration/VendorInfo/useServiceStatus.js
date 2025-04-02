1cfd5d832e940bfb2f4223f327f64174fb6b6872
elsenhans <elsenhans@users.noreply.github.com>
2025-04-02 09:56:04 +0200

    UIEUS-398-check-status-of-counter-api use ky

--- src/components/HarvestingConfiguration/VendorInfo/useServiceStatus.js
+++ src/components/HarvestingConfiguration/VendorInfo/useServiceStatus.js
@@ -1,13 +1,11 @@
 import { useQuery } from 'react-query';
-import { useOkapiKy } from '@folio/stripes/core';
+import ky from 'ky';
 
 const useServiceStatus = (url) => {
-  const ky = useOkapiKy();
-
   const { data, error, isLoading, refetch } = useQuery(
     ['serviceStatus', url],
     async () => {
-      const response = await ky.get(url, { credentials: 'omit', prefixUrl: '' }).json();
+      const response = await ky.get(url, { credentials: 'omit' }).json();
       if (Array.isArray(response) && response.length > 0 && response[0].Service_Active !== undefined) {
         return response[0].Service_Active;
       }
--- src/components/HarvestingConfiguration/VendorInfo/useServiceStatus.test.js
+++ src/components/HarvestingConfiguration/VendorInfo/useServiceStatus.test.js
@@ -7,12 +7,12 @@ import {
   renderHook,
   waitFor,
 } from '@folio/jest-config-stripes/testing-library/react';
-import { useOkapiKy } from '@folio/stripes/core';
+import ky from 'ky';
 
 import useServiceStatus from './useServiceStatus';
 
-jest.mock('@folio/stripes/core', () => ({
-  useOkapiKy: jest.fn(),
+jest.mock('ky', () => ({
+  get: jest.fn(),
 }));
 
 const mockUrl = 'https://api.example.com/status';
@@ -26,32 +26,28 @@ const wrapper = ({ children }) => (
 
 describe('useServiceStatus', () => {
   beforeEach(() => {
-    useOkapiKy.mockClear();
-    useOkapiKy.mockReturnValue({
-      get: jest.fn(() => ({
-        json: () => Promise.resolve([{ Service_Active: true }]),
-      })),
-    });
+    ky.get.mockClear();
   });
 
   it('should fetch and set status correctly if Service_Active = true', async () => {
+    ky.get.mockReturnValueOnce({
+      json: () => Promise.resolve([{ Service_Active: true }]),
+    });
+
     const { result } = renderHook(() => useServiceStatus(mockUrl), { wrapper });
 
     await waitFor(() => {
       result.current.fetchStatus();
     });
 
-    expect(useOkapiKy).toHaveBeenCalled();
     expect(result.current.isLoading).toBeFalsy();
     expect(result.current.status).toBe(true);
     expect(result.current.error).toBeNull();
   });
 
   it('should fetch and set status correctly if Service_Active = false', async () => {
-    useOkapiKy.mockReturnValueOnce({
-      get: jest.fn(() => ({
-        json: () => Promise.resolve([{ Service_Active: false }]),
-      })),
+    ky.get.mockReturnValueOnce({
+      json: () => Promise.resolve([{ Service_Active: false }]),
     });
 
     const { result } = renderHook(() => useServiceStatus(mockUrl), { wrapper });
@@ -65,28 +61,9 @@ describe('useServiceStatus', () => {
     expect(result.current.error).toBeNull();
   });
 
-  it('should handle fetch errors', async () => {
-    useOkapiKy.mockReturnValueOnce({
-      get: jest.fn(() => {
-        throw new Error('Network error');
-      }),
-    });
-
-    const { result } = renderHook(() => useServiceStatus(mockUrl), { wrapper });
-
-    await waitFor(() => {
-      result.current.fetchStatus();
-    });
-
-    expect(result.current.isLoading).toBeFalsy();
-    expect(result.current.error.message).toBe('Network error');
-  });
-
   it('should handle invalid response structure', async () => {
-    useOkapiKy.mockReturnValueOnce({
-      get: jest.fn(() => ({
-        json: () => Promise.resolve({ invalid: 'data' }),
-      })),
+    ky.get.mockReturnValueOnce({
+      json: () => Promise.resolve([{ invalid: 'data' }]),
     });
 
     const { result } = renderHook(() => useServiceStatus(mockUrl), { wrapper });
