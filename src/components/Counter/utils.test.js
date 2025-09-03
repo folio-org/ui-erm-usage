52607ea135c204b25a2d3a5b6dcab6fd0d80165c
elsenhans <elsenhans@users.noreply.github.com>
2025-09-03 10:21:08 +0200

    UIEUS-458-fix-downlod-counter-value-bug

--- src/components/Counter/utils.test.js
+++ src/components/Counter/utils.test.js
@@ -2,23 +2,23 @@ import { getAvailableReports, getDownloadCounterReportTypes } from './utils';
 
 describe('getDownloadCounterReportTypes', () => {
   it('should return correct values for release 4 and report JR1', () => {
-    const expectedValues = [{ value: 'JR1', label: 'JR1 (4)', release: '4' }];
+    const expectedValues = [{ value: 'JR1 (4)', label: 'JR1 (4)', release: '4' }];
     expect(getDownloadCounterReportTypes('4', 'JR1')).toEqual(expectedValues);
   });
 
   it('should return correct values for release 5 and report DR', () => {
     const expectedValues = [
-      { value: 'DR', label: 'DR (5)', release: '5' },
-      { value: 'DR_D1', label: 'DR_D1 (5)', release: '5' },
+      { value: 'DR (5)', label: 'DR (5)', release: '5' },
+      { value: 'DR_D1 (5)', label: 'DR_D1 (5)', release: '5' },
     ];
     expect(getDownloadCounterReportTypes('5', 'DR')).toEqual(expectedValues);
   });
 
   it('should return correct values for release 5.1 and report DR', () => {
     const expectedValues = [
-      { value: 'DR', label: 'DR (5.1)', release: '5.1' },
-      { value: 'DR_D1', label: 'DR_D1 (5.1)', release: '5.1' },
-      { value: 'DR_D2', label: 'DR_D2 (5.1)', release: '5.1' },
+      { value: 'DR (5.1)', label: 'DR (5.1)', release: '5.1' },
+      { value: 'DR_D1 (5.1)', label: 'DR_D1 (5.1)', release: '5.1' },
+      { value: 'DR_D2 (5.1)', label: 'DR_D2 (5.1)', release: '5.1' },
     ];
     expect(getDownloadCounterReportTypes('5.1', 'DR')).toEqual(expectedValues);
   });
