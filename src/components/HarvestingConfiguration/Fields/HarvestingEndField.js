130bb30a0d0ea018d14d76b361c25f0e93405daf
elsenhans <48911833+elsenhans@users.noreply.github.com>
2026-02-10 14:30:39 +0100

    UIEUS-473: Move Monthpicker to stripes-leipzig-components (#604)
    https://folio-org.atlassian.net/browse/UIEUS-473
    
    **1. Step:** Add Monthpicker to `stripes-leipzig-components`:
    Has been done in this PR:
    https://github.com/folio-org/stripes-leipzig-components/pull/11
    
    **2. Step:** Use Monthpicker from `stripes-leipzig-components`:
    - Remove Monthpicker component
    - remove translations for Monthpicker
    - remove `luxon` dependencies and add dependency to
    `stripes-leipzig-components`
    - remove `sonar-project.properties` since there are just rules related
    to Monthpicker
    - use named import for Monthpicker
    
    ---------
    
    Co-authored-by: elsenhans <elsenhans@users.noreply.github.com>

--- src/components/HarvestingConfiguration/Fields/HarvestingEndField.js
+++ src/components/HarvestingConfiguration/Fields/HarvestingEndField.js
@@ -1,6 +1,6 @@
 import { useIntl } from 'react-intl';
 
-import Monthpicker from '../../../util/Monthpicker';
+import { Monthpicker } from '@folio/stripes-leipzig-components';
 
 const HarvestingEndField = () => {
   const intl = useIntl();
@@ -8,7 +8,6 @@ const HarvestingEndField = () => {
   return (
     <Monthpicker
       backendDateFormat="YYYY-MM"
-      id="input-harvestingEnd"
       name="harvestingConfig.harvestingEnd"
       textLabel={intl.formatMessage({ id: 'ui-erm-usage.udpHarvestingConfig.harvestingEnd' })}
     />
