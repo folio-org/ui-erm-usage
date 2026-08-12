712a2ce4aaaabcc54131c8a57730a4da69253cb7
elsenhans <elsenhans@users.noreply.github.com>
2026-08-12 16:53:36 +0200

    UIEUS-537-clean-hardcoded-strings

--- src/components/ReportUpload/NonCounterUpload/NonCounterInnerForm.js
+++ src/components/ReportUpload/NonCounterUpload/NonCounterInnerForm.js
@@ -156,7 +156,7 @@ function NonCounterUploadInnerForm(props) {
               id="custom-report-year"
               label={<FormattedMessage id="ui-erm-usage.general.year" />}
               name="year"
-              placeholder="YYYY"
+              placeholder={intl.formatMessage({ id: 'ui-erm-usage.general.yearFormat' })}
               required
             />
           </Col>
--- src/util/data/harvestingViaOptions.js
+++ src/util/data/harvestingViaOptions.js
@@ -1,5 +1,7 @@
+import { FormattedMessage } from 'react-intl';
+
 export default [
   { value: undefined, label: '' },
-  { value: 'aggregator', label: 'Aggregator' },
+  { value: 'aggregator', label: <FormattedMessage id="ui-erm-usage.information.aggregator" /> },
   { value: 'sushi', label: 'Counter / Sushi' },
 ];
